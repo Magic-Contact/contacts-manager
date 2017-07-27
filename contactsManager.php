1424757662909c574b63f928c64d302c624e85ce
Eric Pharr <ericjpharr@gmail.com>
2017-07-27 10:33:59 -0500

    Added comments/plans for project; Contacts.txt

--- contactsManager.php
+++ contactsManager.php
@@ -1,1 +1,58 @@
 <?php
+
+//funtion to import contacts ---->
+    //fopen()
+    
+    //create array
+        // user can access and change array while program is running
+
+    // fclose()
+
+
+// Menu option variable = 1, 2, 3, 4, 5
+    //program runs until variable = 5
+
+
+//function showMenu() --->
+
+    //Print Menu
+        // View contacts.
+        // Add a new contact.
+        // Search a contact by name.
+        // Delete an existing contact.
+        // Exit. Enter an option (1, 2, 3, 4 or 5)
+
+    //User input
+
+
+//show menu ----->
+    // take in contacts array and output contacts according to format
+
+
+// add contact ---->
+    // push new contact to array
+
+    // check that user input is correct (name, phone number)
+
+//search contact ----->
+    // ask for user input and search for matches in array
+
+    // display result
+
+// delete an existing contact
+    // use search by contact to find contact to delete
+
+    // remove item from array
+
+
+// DO FIRST*****
+
+
+// exit ---------->
+
+    // this is a do-while loop
+
+    // write contact array to contacts file
+
+    //exit program
+
